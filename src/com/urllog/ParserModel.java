95b250ba8f27e52c278740adc23cfcce924a352d
ordinary-student <52038952+ordinary-student@users.noreply.github.com>
2019-07-03 22:54:57 +0800

    Update ParserModel.java

--- src/com/urllog/ParserModel.java
+++ src/com/urllog/ParserModel.java
@@ -1,5 +1,7 @@
 package com.urllog;
 
+import java.util.HashMap;
+
 /**
  * 解析模式
  * 
@@ -8,11 +10,48 @@ package com.urllog;
  */
 public class ParserModel
 {
+	public HashMap<String, Boolean> map = new HashMap<String, Boolean>();
+
 	/*
 	 * 构造方法
 	 */
-	public ParserModel()
+	public ParserModel(String key, boolean value)
 	{
+		put(key, value);
+	}
 
+	public ParserModel(String[] keys, boolean[] values)
+	{
+		put(keys, values);
+	}
+
+	/**
+	 * 添加键值对
+	 * 
+	 * @param key
+	 * @param value
+	 */
+	public void put(String key, boolean value)
+	{
+		// 添加键值对
+		map.put(key, value);
+	}
+
+	/**
+	 * 添加键值对数组
+	 * 
+	 * @param keys
+	 * @param values
+	 */
+	public void put(String[] keys, boolean[] values)
+	{
+		for (int i = 0; i < keys.length; i++)
+		{
+			// 获取键值
+			String key = keys[i];
+			boolean value = values[i];
+			// 添加键值对
+			map.put(key, value);
+		}
 	}
 }
