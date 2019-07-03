c20984df6f157b1c7040f194592482fedb44a920
ordinary-student <52038952+ordinary-student@users.noreply.github.com>
2019-07-03 19:56:32 +0800

    Create UrlLog.java

---
+++ src/com/urllog/UrlLog.java
@@ -0,0 +1,63 @@
+package com.urllog;
+
+/**
+ * 网址解析日志记录类
+ * 
+ * @author ordinary-student
+ *
+ */
+public class UrlLog
+{
+	private String time;
+	private String url;
+
+	public UrlLog()
+	{
+	}
+
+	public UrlLog(String time, String url)
+	{
+		this.time = time;
+		this.url = url;
+	}
+
+	@Override
+	public String toString()
+	{
+		return time + "---" + url;
+	}
+
+	/**
+	 * @return time
+	 */
+	public String getTime()
+	{
+		return time;
+	}
+
+	/**
+	 * @param time
+	 *            要设置的 time
+	 */
+	public void setTime(String time)
+	{
+		this.time = time;
+	}
+
+	/**
+	 * @return url
+	 */
+	public String getUrl()
+	{
+		return url;
+	}
+
+	/**
+	 * @param url
+	 *            要设置的 url
+	 */
+	public void setUrl(String url)
+	{
+		this.url = url;
+	}
+}
